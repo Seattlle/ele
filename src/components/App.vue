00bf79003326d69fcadd559578877d70578a6534
Seattle <727112594@qq.com>
2018-03-11 15:57:34 +0800

    规范化webpack.config样式

--- src/components/App.vue
+++ src/components/App.vue
@@ -55,13 +55,6 @@
              }
         }
     }
-    function resize(){
-        var _width=window.innerWidth>750?750:window.innerWidth;
-        var fontSize=_width/7.5;
-        document.documentElement.style.fontSize=fontSize+"px";
-    }
-    resize();
-    window.onresize=resize;
 </script>
 <style>
     @import "../css/base.css";
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,116 @@
 const  webpack=require('webpack');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
+const CleanWebpackPlugin=require('clean-webpack-plugin');
+/*
+* 系统当前运行命令
+* */
+var currentTarget=process.env.npm_lifecycle_event;
 
-module.exports = {
-    devtool: 'source-map',
-
-    entry:  __dirname + "/src/components/main.js",//已多次提及的唯一入口文件
-    output: {
-        path: __dirname + "/build/",//打包后的文件存放的地方
-        filename: "[name].js"//打包后输出文件的文件名
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options:{
-                    loaders:{
-                        js:'babel-loader'
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|eot|woff|ttf)(\?.*)?$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 50000
-                }
-            },{
-                test:/\.css$/,
-                loader:'style-loader!css-loader?modules'
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:'./index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                devServer: {
-                    contentBase: "./app",//本地服务器所加载的页面所在的目录
-                    colors: true,//终端中输出结果为彩色
-                    historyApiFallback: true,//不跳转
-                    inline: true,//实时刷新
-                    port:8099
-                }
-            }
-        }),
-
-        new webpack.optimize.UglifyJsPlugin({ // js、css都会压缩
-            mangle: {
-                except: ['$super', '$', 'exports', 'require', 'module', '_']
-            },
-            compress: {
-                warnings: false
-            },
-            output: {
-                comments: false,
-            }
-        })
-    ]
+var devServer=false;
+if(currentTarget=="dev"){
+    devServer=true;
 }
+
+/**
+ *主文件入口
+ */
+var entry={
+    index:__dirname + "/src/components/main.js",
+    common:[
+        __dirname + "/src/javascript/base.js"
+    ]
+};
+
+/**
+ * 文件输出
+ * * */
+var output={
+  path:__dirname+"/build/",
+  // filename:devServer?'[name].js':'[name]-[chunkhash].js'
+  filename:'[name].js'
+};
+
+/**
+* 编译组件
+* */
+var loaders=[
+    {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options:{
+            loaders:{
+                js:'babel-loader'
+            }
+        }
+    },
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+    },
+    {
+        test: /\.(png|jpe?g|gif|svg|eot|woff|ttf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+            limit: 50000
+        }
+    },
+    {
+        test:/\.css$/,
+        loader:'style-loader!css-loader?modules'
+    }
+];
+
+var plugins=[
+    new HtmlWebpackPlugin({
+        template:'./index.html'
+    }),
+
+   // js、css都会压缩
+    new webpack.optimize.UglifyJsPlugin({
+        mangle: {
+            except: ['$super', '$', 'exports', 'require', 'module', '_']
+        },
+        compress: {
+            warnings: false
+        },
+        output: {
+            comments: false,
+        }
+    }),
+
+    /*
+      * Using this config the vendor chunk should not be changing its hash unless you change its code or dependencies
+      * （避免在文件不改变的情况下hash值不变化）
+      * */
+    new webpack.optimize.OccurrenceOrderPlugin()
+];
+
+if(devServer){
+    /*
+    * 热更新
+    * */
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+}else{
+    /*
+    * clean publishing directory
+    * （发布前清空发布目录）
+    * */
+    plugins.push(new CleanWebpackPlugin(['build'], {
+        root: '', // An absolute path for the root  of webpack.config.js
+        verbose: true,// Write logs to console.
+        exclude:["indexSlideType.js","shopInfo.js","shopList.js"],
+        dry: false // Do not delete anything, good for testing.
+    }))
+}
+
+var config={
+    entry:entry,
+    output:output,
+    module: {
+        loaders: loaders
+    },
+    plugins: plugins
+};
+
+module.exports = config;
